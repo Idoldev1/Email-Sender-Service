253a60691107a4c526c057108bdce232dbaeb394
Idoldev1 <idolayinka98@gmail.com>
2024-01-30 16:05:49 +0100

    Additional changes to repository

--- Email-Sender-API/MailRepository/MailRequest.cs
+++ Email-Sender-API/MailRepository/MailRequest.cs
@@ -6,5 +6,5 @@ public class MailRequest
     public string RecipientMail { get; set; }
     public string Subject { get; set; }
     public string Body { get; set; }
-    public List<IFormFile> Attachments { get; set; }
+    //public List<IFormFile> Attachments { get; set; }
 }
