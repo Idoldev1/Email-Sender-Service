e252add483e15b958ac8783733b1819237de482b
Idoldev1 <idolayinka98@gmail.com>
2024-01-31 09:29:23 +0100

    Corrected Username and password for connection

--- Email-Sender-Service/Email-Sender-API/Services/IMailServices.cs
+++
@@ -1,9 +0,0 @@
-using Email_Sender_API.MailRepository;
-
-namespace Email_Sender_API.Services;
-
-
-public interface IMailServices
-{
-    Task SendMailAsync(MailRequest mailRequest);
-}
--- Email-Sender-Service/README.md
+++
@@ -1,2 +0,0 @@
-# Email-Sender-Service
-Its a basic email sender service that lets you send email using features from the .NET framework
